87269e3284634a3a486485d10075c04833f6b9a3
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-06 15:56:39 +0000

    I made the changes on 2023-09-06 15:56:39

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+def print_last_digit(number):
+    number = str(number)
+    if (number.isnumeric() is True or number[0] == "-"):
+        print(number[-1], end="")
+        return (number[-1])
+    else:
+        print("TypeError")
