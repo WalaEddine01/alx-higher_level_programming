f8ca9f0b84c873c359fe34bc086d4a1f5cf96c49
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-09 10:19:18 +0000

    I made the changes on 2023-09-09 10:19:18

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 1 and len(tuple_b) >= 2:
-        return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
-    elif len(tuple_b) == 1 and len(tuple_a) >= 2:
-        return (tuple_b[0] + tuple_a[0], 0 + tuple_a[1])
-    elif len(tuple_b) == 1 and len(tuple_a) == 1:
-        return (tuple_a[0] + tuple_b[0], 0 + 0)
-    elif len(tuple_a) >= 2 and len(tuple_b) == 0:
-        return (tuple_a[0],tuple_a[1])
-    elif len(tuple_b) >= 2 and len(tuple_a) == 0:
-        return (tuple_b[0],tuple_b[1])
-    elif len(tuple_a) == 1 and len(tuple_b) == 0:
-        return (tuple_a[0],0)
-    elif len(tuple_b) == 1 and len(tuple_a) == 0:
-        return (tuple_b[0],0)
-    elif len(tuple_b) == 0 and len(tuple_a) == 0:
-        return ()
-    else:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    aa1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    aa2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    bb1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    bb2 = tuple_b[1] if len(tuple_b) > 1 else 0
+    return (aa1 + bb1, aa2 + bb2)
