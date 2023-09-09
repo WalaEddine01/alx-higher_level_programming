161e3fef0b6ee6c240c9d5b88c9d2920043b06df
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-09 20:41:10 +0000

    I made the changes on 2023-09-09 20:41:10

--- 0x03-python-data_structures/main.c
+++ 0x03-python-data_structures/main.c
@@ -21,7 +21,7 @@ int main(void)
     add_nodeint_end(&head, 50);
     add_nodeint_end(&head, 972);
     add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
     print_listint(head);
 
     if (is_palindrome(&head) == 1)
