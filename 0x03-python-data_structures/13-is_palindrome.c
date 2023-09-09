e12e7cd10a2960c87c11eb01816d31a7cf1c048b
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-09 19:03:24 +0000

    I made the changes on 2023-09-09 19:03:24

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks if the singly linked list is plaindrome
+ * @head: pointer the head of the singly linked list
+ * Return: 1 if it is plainddrom 0 if it is not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *list1, *list2;
+	int i, j, n = 0;
+
+	if (head == NULL && (*head)->next == NULL)
+		return (1);
+	list1 = *head;
+	while (list1)
+	{
+		n++;
+		list1 = list1->next;
+	}
+	for (i = 0; i < n / 2; i++)
+	{
+		list1 = *head;
+		list2 = *head;
+		for (j = 0; j < n - i - 1; j++)
+			list1 = list1->next;
+		for (j = 0; j < i; j++)
+			list2 = list2->next;
+		if (list1->n != list2->n)
+			return (0);
+	}
+	return (1);
+}
---
+++ 0x03-python-data_structures/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+
+    if (is_palindrome(&head) == 1)
+        printf("Linked list is a palindrome\n");
+    else
+        printf("Linked list is not a palindrome\n");
+
+    free_listint(head);
+
+    return (0);
+}
