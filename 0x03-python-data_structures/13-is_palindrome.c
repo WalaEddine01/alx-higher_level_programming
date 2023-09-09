e79241ff41bb74cd82f6d42a2914b48ebfccc8fb
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-09 20:27:15 +0000

    I made the changes on 2023-09-09 20:27:14

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,17 +1,15 @@
 #include "lists.h"
 /**
- * is_palindrome - checks if the singly linked list is plaindrome
+ * check_loop - checks if the singly linked list is plaindrome
  * @head: pointer the head of the singly linked list
  * Return: 1 if it is plainddrom 0 if it is not
  */
-int is_palindrome(listint_t **head)
+int check_loop(listint_t *head)
 {
 	listint_t *list1 = NULL, *list2 = NULL;
 	int i, j, n = 0;
 
-	if (head == NULL && (*head)->next == NULL && *head == NULL)
-		return (1);
-	list1 = *head;
+	list1 = head;
 	while (list1)
 	{
 		n++;
@@ -19,20 +17,28 @@ int is_palindrome(listint_t **head)
 	}
 	for (i = 0; i < n / 2; i++)
 	{
-		list1 = *head;
-		list2 = *head;
+		list1 = head;
+		list2 = head;
 		for (j = 0; j < n - i - 1; j++)
 			list1 = list1->next;
 		for (j = 0; j < i; j++)
 			list2 = list2->next;
 		if (list1->n != list2->n)
-		{
-			list1 = NULL;
-			list2 = NULL;
-		}
-		return (0);
+			return (0);
 	}
-	list1 = NULL;
-	list2 = NULL;
 	return (1);
+}
+/**
+ * is_palindrome - checks if the singly linked list is plaindrome
+ * @head: pointer the head of the singly linked list
+ * Return: 1 if it is plainddrom 0 if it is not
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL && (*head)->next == NULL && *head == NULL)
+		return (1);
+	if (check_loop(*head) == 1)
+		return (1);
+	else
+		return (0);
 }
--- 0x03-python-data_structures/main.c
+++ 0x03-python-data_structures/main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     listint_t *head;
 
-        head = NULL;
+    head = NULL;
     add_nodeint_end(&head, 1);
     add_nodeint_end(&head, 17);
     add_nodeint_end(&head, 972);
@@ -21,9 +21,8 @@ int main(void)
     add_nodeint_end(&head, 50);
     add_nodeint_end(&head, 972);
     add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 1);
     print_listint(head);
-
 
     if (is_palindrome(&head) == 1)
         printf("Linked list is a palindrome\n");
