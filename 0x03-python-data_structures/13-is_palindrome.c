d3cac367a70c33ca6d16784fc75be45d5c929564
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-10 14:13:40 +0000

    I made the changes on 2023-09-10 14:13:40

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 /**
- * check_loop - checks if the singly linked list is plaindrome
+ * is_palindrome - checks if the singly linked list is plaindrome
  * @head: pointer the head of the singly linked list
  * Return: 1 if it is plainddrom 0 if it is not
  */
-int check_loop(listint_t *head)
+int is_palindrome(listint_t **head)
 {
 	listint_t *list1 = NULL, *list2 = NULL;
 	int i, j, n = 0;
 
-	list1 = head;
+	if (head == NULL && (*head)->next == NULL && *head == NULL)
+		return (1);
+	list1 = *head;
 	while (list1)
 	{
 		n++;
@@ -17,28 +19,14 @@ int check_loop(listint_t *head)
 	}
 	for (i = 0; i < n / 2; i++)
 	{
-		list1 = head;
-		list2 = head;
+		list1 = *head;
+		list2 = *head;
 		for (j = 0; j < n - i - 1; j++)
 			list1 = list1->next;
 		for (j = 0; j < i; j++)
 			list2 = list2->next;
-		if (i >= n -1 || list1->n != list2->n)
+		if (list1->n != list2->n)
 			return (0);
 	}
 	return (1);
-}
-/**
- * is_palindrome - checks if the singly linked list is plaindrome
- * @head: pointer the head of the singly linked list
- * Return: 1 if it is plainddrom 0 if it is not
- */
-int is_palindrome(listint_t **head)
-{
-	if (head == NULL && (*head)->next == NULL && *head == NULL)
-		return (1);
-	if (check_loop(*head) == 1)
-		return (1);
-	else
-		return (0);
 }
