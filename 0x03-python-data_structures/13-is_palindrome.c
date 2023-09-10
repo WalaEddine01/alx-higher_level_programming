3f8fc0320cb6ce81efd2bbe80c0f1d13c9eef7ca
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-10 15:54:48 +0000

    I made the changes on 2023-09-10 15:54:48

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -17,7 +17,6 @@ int is_palindrome(listint_t **head)
 		n++;
 		list1 = list1->next;
 	}
-	list1 = *head;
 	for (i = 0; i < n / 2; i++)
 	{
 		list1 = *head;
@@ -26,7 +25,7 @@ int is_palindrome(listint_t **head)
 			list2 = list2->next;
 		for (j = 0; j < n - i - 1; j++)
 			list1 = list1->next;
-		if (list1->n != list2->n || i <= 8)
+		if (list1->n != list2->n)
 			return (0);
 	}
 	return (1);
