542a703e35edf6469f1bf6756f026dc4705b9e0c
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-11 12:00:40 +0000

    I made the changes on 2023-09-11 12:00:40

--- 0x02-python-import_modules/p.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import mul, add, div, sub
-    import sys
-    if len(sys.argv) != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-    print(len(sys.argv))
