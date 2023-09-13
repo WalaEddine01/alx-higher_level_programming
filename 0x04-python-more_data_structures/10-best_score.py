51cab5c3a2e321334b53238b03a5322bf6bb64fb
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-13 21:22:31 +0000

    I made the changes on 2023-09-13 21:22:31

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        a = None
-    else:
-        maxe = 0
-        a = 0
-        for i, j in a_dictionary.items():
-            if j > maxe:
-                maxe = j
-                a = i
+        return None
+    maxe = 0
+    a = 0
+    for i, j in a_dictionary.items():
+        if j > maxe:
+            maxe = j
+            a = i
     return a
