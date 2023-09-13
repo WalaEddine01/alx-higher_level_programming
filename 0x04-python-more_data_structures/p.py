4a65cef47b0ac7f536bb7e1ba7fd69a3c5c9f8f9
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-13 20:27:36 +0000

    I made the changes on 2023-09-13 20:27:36

--- 0x04-python-more_data_structures/p.py
+++ 0x04-python-more_data_structures/p.py
@@ -4,15 +4,5 @@ print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_d
 
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
-
-print("--")
-print("--")
-
 new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
-
+print(new_dict)
