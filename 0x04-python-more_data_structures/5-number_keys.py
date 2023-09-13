02e0d7c248fb3c44425be4a18f1c4a646f710ee3
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-13 19:37:49 +0000

    I made the changes on 2023-09-13 19:37:49

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-
--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -3,4 +3,5 @@ only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
 set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" }
 c_set =  only_diff_elements(set_1, set_2)
+
 print(sorted(list(c_set)))
