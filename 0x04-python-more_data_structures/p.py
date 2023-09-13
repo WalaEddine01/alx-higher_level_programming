65c8940d5fe96ddcfe903307c9535f2062cc5318
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-13 21:29:28 +0000

    I made the changes on 2023-09-13 21:29:28

--- 0x04-python-more_data_structures/p.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-update_dictionary = __import__('7-update_dictionary').update_dictionary
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print(new_dict)
