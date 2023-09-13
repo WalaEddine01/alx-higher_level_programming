1f35bddb09e0a655310660e635706d300923945e
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-13 21:14:02 +0000

    I made the changes on 2023-09-13 21:14:02

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,10 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        return max(a_dictionary.keys())
+        maxe = 0
+        a = 0
+        for i in a_dictionary:
+            if a_dictionary[i] > maxe:
+                maxe = a_dictionary[i]
+                a = i
+        return a
