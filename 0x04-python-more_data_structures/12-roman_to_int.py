8151e3134ad4b2232e01841d2905ee27c3c5e2a3
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-15 15:26:23 +0000

    I made the changes on 2023-09-15 15:26:23

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
-        return None
+        return 0
     a = 0
     number = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     for i in range(len(roman_string)):
@@ -11,6 +11,4 @@ def roman_to_int(roman_string):
                 if i != 0 and \
                         number[roman_string[i]] > number[roman_string[i - 1]]:
                     a -= number[roman_string[i - 1]] * 2
-    if a == 0:
-        return None
     return a
--- 0x04-python-more_data_structures/a.py
+++ 0x04-python-more_data_structures/a.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 roman_to_int = __import__('12-roman_to_int').roman_to_int
-roman_number = "IX"
+roman_number = 9
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
