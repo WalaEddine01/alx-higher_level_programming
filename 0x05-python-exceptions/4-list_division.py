4a3d7953d2dea135479e0ea3eb20a24b1d19c726
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-25 15:50:14 +0000

    I made the changes on 2023-09-25 15:50:14

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,8 +13,8 @@ def list_division(my_list_1, my_list_2, list_length):
             except TypeError:
                 print("wrong type")
                 new_list.append(0)
-            except ValueError:
-                new_list.append(0)
+            finally:
+                result = 0
     except IndexError:
         print("out of range")
         new_list.append(0)
