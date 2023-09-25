da707d3d2ea87dd960444d2eeeb30d3ef57eccd3
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-25 18:21:45 +0000

    I made the changes on 2023-09-25 18:21:45

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as te:
-        print(te, file=sys.stderr)
+        print("Exception: {}".format(te), file=sys.stderr)
         return False
