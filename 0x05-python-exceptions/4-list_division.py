677ae33301e48a817634db97765102000ff47f86
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-25 16:01:18 +0000

    I made the changes on 2023-09-25 16:01:18

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(0, list_length):
         try:
-        new_list.append(my_list_1[i] / my_list_2[i])
+            new_list.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             print("division by 0")
             new_list.append(0)
