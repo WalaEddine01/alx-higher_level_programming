bc5102d0d1dd0d6366ae61d274a9a187d9dd51a7
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-25 15:44:32 +0000

    I made the changes on 2023-09-25 15:44:32

--- 0x05-python-exceptions/z.py
+++ 0x05-python-exceptions/z.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+list_division = __import__('4-list_division').list_division
 
-my_list = [1, 2, [1, 2, 3]]
-for j in range(0, 3):
-    print("{:d}".format(my_list[j]), end="")
+my_l_1 = [10, 8, 4]
+my_l_2 = [2, 4, 4]
+result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+print(result)
