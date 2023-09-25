b1532d05318cff492d8b8286ac532f31cdb807e3
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-25 16:04:12 +0000

    I made the changes on 2023-09-25 16:04:12

--- 0x05-python-exceptions/z.py
+++ 0x05-python-exceptions/z.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-list_division = __import__('4-list_division').list_division
-
-my_l_1 = [10, 8, 4]
-my_l_2 = [2, 4, [1, 3, 4]]
-result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-print(result)
+try:
+    raise TypeError  # This will raise a TypeError
+except TypeError as te:
+    print("An error occurred:")
