42d289451cb4f0dbead94f43d9399a6fdd8725f9
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-25 08:32:42 +0000

    I made the changes on 2023-09-25 08:32:42

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
-my_list = ["a", 2, 9, True, 5]
+safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
---
+++ 0x05-python-exceptions/z.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+x = "d"
+print("{:d}".format(x))
