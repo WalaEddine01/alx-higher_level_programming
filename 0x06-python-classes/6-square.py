28d918f147adaa97c9dbbc147dfa5586644f6399
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-27 20:26:37 +0000

    I made the changes on 2023-09-27 20:26:37

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,6 +15,8 @@ class Square:
     __position = (0, 0)
 
     def __init__(self, size=0, position=(0, 0)):
+        if (len(position) != 2):
+            raise IndexError("Index out of the range")
         if (type(size) is int):
             if size < 0:
                 raise ValueError("size must be >= 0")
@@ -26,8 +28,7 @@ class Square:
                 or type(position[1]) is not int or len(position) != 2
                 or position[1] < 0 or position[0] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = position
+        self.__position = position
 
     def area(self):
         """
@@ -109,10 +110,8 @@ class Square:
             value (tuple): position
         """
         if (type(value) is not tuple or type(value[0]) is not int
-                or type(value[1]) is not int
+                or type(value[1]) is not int or len(value) != 2
                 or value[1] < 0 or value[0] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(value) != 2:
-            raise Exception("out of the range-----")
         else:
             self.__position = value
