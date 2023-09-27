87b5b295ef3c4925c39e4255ea3d1413671c84ee
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-27 10:20:42 +0000

    I made the changes on 2023-09-27 10:20:42

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This moduale defines a empty class for working wit square
+"""
+
+
+class Square:
+    """
+    This class represents a square
+
+    Attributes:
+    (No)
+    """
+    def __init__(self):
+        """
+        Initializes a new square.
+        Args:
+        (NO)
+        """
+        pass
