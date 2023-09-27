b9d035911c76827d7e779cb4e0a7b1ade1c421b4
WalaEddine01 <walaaeddine33@gmail.Com>
2023-09-27 20:11:34 +0000

    I made the changes on 2023-09-27 20:11:34

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -74,6 +74,11 @@ class Square:
 
         Return: Nothing
         """
+        if (type(self.__position) is not tuple or type(self.__position[0]) is not int
+                or type(self.__position[1]) is not int or len(self.__position) != 2
+                or self.__position[1] < 0 or self.__position[0] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+            return
         j = self.__size
         a = self.__position[1]
         if j == 0:
@@ -109,8 +114,10 @@ class Square:
             value (tuple): position
         """
         if (type(value) is not tuple or type(value[0]) is not int
-                or type(value[1]) is not int or len(value) != 2
+                or type(value[1]) is not int
                 or value[1] < 0 or value[0] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            raise Exception("out of the range-----")
         else:
             self.__position = value
