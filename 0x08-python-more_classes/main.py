1349c2ef40b49ceef1cdbfd930707e48cea36b31
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-02 08:58:31 +0000

    I made the changes on 2023-10-02 08:58:31

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-Rectangle = __import__('0-rectangle').Rectangle
+Rectangle = __import__('1-rectangle').Rectangle
 
-my_rectangle = Rectangle()
-print(type(my_rectangle))
+my_rectangle = Rectangle(2, 4)
 print(my_rectangle.__dict__)
+
+my_rectangle.width = "f"
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
+
