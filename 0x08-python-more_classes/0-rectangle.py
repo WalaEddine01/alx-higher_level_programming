9a13a26ca68f76cd297f69a10ee308b1d4350716
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-02 08:09:27 +0000

    I made the changes on 2023-10-02 08:09:26

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This Modual for Rectangle class and Its operation
+"""
+
+
+class Rectangle:
+    """
+    This class represents a sRectangle
+
+    Attributes:
+    (No)
+    """
+    def __init__(self):
+        """
+        Initialize a new Rectangle
+
+        Args:
+            (NO)
+        """
+        pass
