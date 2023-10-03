ec534f1ee1628ac3291cbc39a2359db596489eb8
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-03 21:33:28 +0000

    I made the changes on 2023-10-03 21:33:28

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 class LockedClass:
+    """
+    This class contains method that prevent the user to create
+    object but the ones with name 'first_name'
+    """
     __slots__ = "first_name"
