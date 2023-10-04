9d8180f4dc13014f3564dcf25e2a67f6dc381283
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-04 07:18:28 +0000

    I made the changes on 2023-10-04 07:18:28

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+This is say_my_name modual contains ay_my_name function
+"""
 def say_my_name(first_name, last_name=""):
+    """
+    this is ay_my_name function prints My name is {first_name} {last_name}
+    message
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
