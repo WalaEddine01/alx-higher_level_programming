e7989f30e4a43a0f9f35a528039f852b89d119f6
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-04 07:14:45 +0000

    I made the changes on 2023-10-04 07:14:45

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,6 +5,12 @@ This modual contains print_square function
 
 
 def print_square(size):
+    """
+    This function prints a square with "#"
+
+    Args:
+        size (int): the size of the square
+    """
     if type(size) is not int or type(size) is float and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
