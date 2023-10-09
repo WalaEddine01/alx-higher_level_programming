55fec5f7ff9683a5beb0a37e20788b47c3774ca2
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-09 20:06:36 +0000

    I made the changes on 2023-10-09 20:06:36

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -4,7 +4,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 bg = BaseGeometry()
 
 bg.integer_validator("my_int", 12)
-bg.integer_validator("width", 89)
+bg.integer_validator("d", 89)
 
 try:
     bg.integer_validator("name", "John")
