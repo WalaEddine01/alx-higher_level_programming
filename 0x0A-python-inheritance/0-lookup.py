78ca1028b36d292680aee0e84e2a5e68c0a7c9af
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-09 09:55:28 +0000

    I made the changes on 2023-10-09 09:55:28

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This Modual contains the lookup function
+"""
+def lookup(obj):
+    """
+    This function returns the list of available attributes and methods of an
+    object
+
+    Args:
+        obj: it is a object
+
+    Return: returns the list of available attributes and methods of an object
+    """
+    return dir(obj)
