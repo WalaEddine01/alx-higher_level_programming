2fbdc358b6811fa0abaedb13e34fb56eda2ab750
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-09 14:16:07 +0000

    I made the changes on 2023-10-09 14:16:07

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -2,11 +2,9 @@
 MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(44)
-my_list.append(5)
+my_list.append("-1")
+my_list.append(-2)
+my_list.append(-3)
 print(my_list)
 my_list.print_sorted()
 print(my_list)
