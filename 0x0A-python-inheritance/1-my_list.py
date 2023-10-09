0e93384e76c90705ddb1c0570ff4219b4d18a304
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-09 13:52:10 +0000

    I made the changes on 2023-10-09 13:52:10

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+This Modual contains the lookup class with its properties
+"""
+
+
+class MyList(list):
+    """
+    This is MyList class that that inherits from list
+    """
+    def __init__(self):
+        """
+        this is the consteructore of MyList class
+        """
+        super().__init__(self)
+
+    def print_sorted(self):
+        """
+        This is print_sorted method of a class
+        that prints the list, but sorted (ascending sort)
+        """
+        a = self.copy()
+        a.sort()
+        print(a)
