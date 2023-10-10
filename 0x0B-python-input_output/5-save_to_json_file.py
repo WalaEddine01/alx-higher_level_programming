7ce38fae2d570fa7219adf324c9f571efb4ceaec
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-10 20:19:41 +0000

    I made the changes on 2023-10-10 20:19:41

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Thid modual contains the from_json_string functioon
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    This function that writes an Object to a text file, using a JSON representation
+
+    Args:
+        my_obj: the text
+        filename: the name of the file
+    """
+    import json
+
+    with open(filename, encoding="utf-8", mode="w") as f:
+        json.dump(my_obj, f)
