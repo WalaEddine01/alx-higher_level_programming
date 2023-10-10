30854dff67727bbf14e9d8386028e28e329a4283
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-10 15:35:28 +0000

    I made the changes on 2023-10-10 15:35:28

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Thid modual contains the read_file functioon
+"""
+
+
+def read_file(filename=""):
+    """
+    this function reads a file and print its content
+
+    Args:
+        filename: the name if the file
+    """
+    with open(filename, encoding="utf-8", mode="r") as f:
+        file_ = f.read()
+    print(file_)
