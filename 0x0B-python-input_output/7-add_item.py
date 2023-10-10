eaab2c61f2d719e0c0e329847d8640cf18b08994
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-10 21:09:56 +0000

    I made the changes on 2023-10-10 21:09:56

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,6 @@
 THIS Modual contains script  that adds all arguments to a
 Python list, and then save them to a file
 """
-import json
 import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
