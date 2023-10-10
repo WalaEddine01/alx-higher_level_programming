0515a92d09e97ff318c2f17d1121d06838003e89
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-10 20:23:23 +0000

    I made the changes on 2023-10-10 20:23:23

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,13 +6,13 @@ Thid modual contains the from_json_string functioon
 
 def save_to_json_file(my_obj, filename):
     """
-    This function that writes an Object to a text file, using a JSON representation
+    This function that writes an Object to a text file, using
+    a JSON representation
 
     Args:
         my_obj: the text
         filename: the name of the file
     """
     import json
-
     with open(filename, encoding="utf-8", mode="w") as f:
         json.dump(my_obj, f)
