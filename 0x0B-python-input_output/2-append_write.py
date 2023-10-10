18b99d7200e32340a2e7153aa8aada7756fde56d
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-10 17:12:34 +0000

    I made the changes on 2023-10-10 17:12:34

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,14 +4,14 @@ Thid modual contains the read_file functioon
 """
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """
-    this function writes a file and return the number of the characters
+    this function append a file and return the number of the characters
 
     Args:
         filename: the name if the file
         text: the text have to write on the file
     """
-    with open(filename, encoding="utf-8", mode="w") as f:
+    with open(filename, encoding="utf-8", mode="a") as f:
         a = f.write(text)
     return a
