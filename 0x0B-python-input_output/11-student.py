21cb7b9786a82d28bd8bcfeae4b0d02e130cda8b
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-11 07:50:13 +0000

    I made the changes on 2023-10-11 07:50:13

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This modual contains a Student class
+"""
+
+
+class Student:
+    """
+    this is student class
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        dict_ = {}
+        """
+        to_json function returns a dictionary of a student instance
+        """
+        if attrs is None:
+            return {"age": self.age, "last_name": self.last_name,
+                    "first_name": self.first_name}
+        else:
+            for i in attrs:
+                if hasattr(self, i):
+                    dict_[i] = getattr(self, i)
+        return dict_
+    def reload_from_json(self, json):
+        '''
+        reload_from_json function that repalce all
+        attributes of the Student
+        '''
+        for key, value in json:
+            setattr(self, key, value)
