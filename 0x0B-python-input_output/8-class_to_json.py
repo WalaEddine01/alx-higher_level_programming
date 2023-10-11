e3d9b2e67adee54b1967ccf25448805e8635e6ce
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-11 06:25:56 +0000

    I made the changes on 2023-10-11 06:25:56

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,8 +2,6 @@
 """
 This modual contains the calss_tojson function
 """
-from_json_string = __import__("4-from_json_string").from_json_string
-e = __import__("e").ab
 
 
 def class_to_json(obj):
@@ -13,5 +11,4 @@ def class_to_json(obj):
     Args:
         obj: the class obj
     """
-    data = e(obj.__dict__)
-    return from_json_string(data)
+    return obj.__dict__
--- 0x0B-python-input_output/e.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-import json
-
-def ab(obj):
-    return json.dumps(obj)
