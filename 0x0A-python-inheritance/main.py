898b939bcf24a729889e1d0a2a8b9e65bd5fba2c
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-11 17:21:33 +0000

    I made the changes on 2023-10-11 17:21:33

--- 0x0A-python-inheritance/main.py
+++ 0x0A-python-inheritance/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 add_attribute = __import__('101-add_attribute').add_attribute
 
 class MyClass():
@@ -14,3 +15,4 @@ try:
     print(a.name)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
+
