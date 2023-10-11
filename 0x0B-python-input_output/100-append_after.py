8d47c067bc594547c86d8c000b1264894a6ce220
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-11 20:19:08 +0000

    I made the changes on 2023-10-11 20:19:08

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,10 +2,12 @@
 """
 This modual contains append_after function
 """
+
+
 def append_after(filename="", search_string="", new_string=""):
     """
     append_after function that inserts a line of text to a file,
-    after each line containing a specific string 
+    after each line containing a specific string
 
     Args:
         filename: the name of the file
@@ -13,15 +15,10 @@ def append_after(filename="", search_string="", new_string=""):
         new_string: the new string
     """
     with open(filename, encoding="utf-8", mode="r") as f:
-        a = 0
         data = f.readlines()
-        for i in range(len(data)):
-            i += 1
-            if a == 1:
-                data.insert(i, new_string)
-                a = 0
-            elif search_string in data[i]:
-                a = 1
+
     with open(filename, encoding="utf-8", mode="w") as f:
-        my_string = ''.join(data)
-        f.write(my_string)
+        for line in data:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
