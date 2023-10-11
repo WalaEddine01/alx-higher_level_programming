8022a8e8073be58c5f9cc5a36a2cee6d19d58908
WalaEddine01 <walaaeddine33@gmail.Com>
2023-10-11 05:49:07 +0000

    I made the changes on 2023-10-11 05:49:07

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,10 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 list_ = []
+try:
+    list_ = load_from_json_file("add_item.json")
+except Exception:
+    list_ = []
 for i in range(1, len(sys.argv)):
     list_.append(sys.argv[i])
 save_to_json_file(list_, "add_item.json")
-load_from_json_file("add_item.json")
