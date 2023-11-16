a39ec2ac08cf1ca909a7dcdf42d783ee4692f2e9
WalaEddine01 <walaaeddine33@gmail.Com>
2023-11-16 16:12:38 +0000

    I made the changes on 2023-11-16 16:12:38

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table second_table of the database
 -- hbtn_0c_0 in your MySQL server.
-SELECT score, name IF EXISTS FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name != "" ORDER BY score DESC;
