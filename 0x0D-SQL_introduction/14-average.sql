1e901a80702e51984b4864ba72d8fe42f843df1a
WalaEddine01 <walaaeddine33@gmail.Com>
2023-11-16 15:44:41 +0000

    I made the changes on 2023-11-16 15:44:41

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,6 +1,3 @@
 -- script that computes the score average of all records
 -- in the table second_table of the database hbtn_0c_0 in your MySQL server.
--- ALTER TABLE second_table ADD COLUMN average FLOAT;
-
--- Upsate the average score as a new record.
-UPDATE second_table set average = (SELECT AVG(score) FROM second_table);
+SELECT AVG(score) AS average FROM second_table;
