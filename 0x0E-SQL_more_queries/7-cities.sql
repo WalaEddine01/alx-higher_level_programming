eb747fc9cbf57135fc5011b35ad6207b1f0c3840
WalaEddine01 <walaaeddine33@gmail.Com>
2023-11-17 20:08:29 +0000

    I made the changes on 2023-11-17 20:08:29

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+	FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states (id),
 	name VARCHAR(256) NOT NULL
 );
