d3e13e7b98397d08971b52619f10d20303286f18
WalaEddine01 <walaaeddine33@gmail.Com>
2023-11-17 15:32:46 +0000

    I made the changes on 2023-11-17 15:32:46

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 -- (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMERY KEY,
+	id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMERY KEY,
 	state_id INT NOT NULL,
 	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 	name VARCHAR(256) NOT NULL
