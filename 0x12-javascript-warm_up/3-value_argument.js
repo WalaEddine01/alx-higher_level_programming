627d5a0e3f13b242a1c7f9eadaa36a67a1d708b7
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-12 21:59:33 +0100

    I made the changes on 2023-12-12 21:59:33

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
+let i = 2;
 const { argv } = require('process');
 if (argv[2] == null) {
   console.log('No argument');
 } else {
-  let i = 2;
   while (argv[i] != null) {
     console.log(argv[i]);
     i++;
