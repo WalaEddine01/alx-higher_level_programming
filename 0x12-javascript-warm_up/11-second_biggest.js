a59bc972e5433ad7b10f725d36af875245cbc1eb
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-12 23:45:20 +0100

    I made the changes on 2023-12-12 23:45:20

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const argv = process.argv;
 if (argv.length < 4) {
   console.log(0);
 } else {
-  let max1 = argv[2]; let max2 = argv[2]; let i = 3;
+  let max1 = parseInt(argv[2]); let max2 = 0; let i = 3;
   while (i < argv.length) {
     if (parseInt(argv[i]) > parseInt(max1)) {
       max2 = max1;
