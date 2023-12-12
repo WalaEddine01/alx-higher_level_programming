99baef283ac96c28204addb79fee2f4a56c67b05
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-12 22:02:54 +0100

    I made the changes on 2023-12-12 22:02:54

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-let i = 2;
 const args = process.argv;
-if (args[2] == undefined) {
+if (args[2] === undefined) {
   console.log('No argument');
 } else {
-  while (args[i] != undefined) {
-    console.log(args[i]);
-    i++;
-  }
+    console.log(args[2]);
 }
