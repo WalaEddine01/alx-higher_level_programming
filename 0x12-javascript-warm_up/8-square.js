acf7c4c94c8ca02526ed79fc87882f2dc80a5e76
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-12 22:58:42 +0100

    I made the changes on 2023-12-12 22:58:42

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const argv = process.argv;
+if (isNaN(Number(argv[2]))) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  const j = 0;
+  let x = '';
+  while (i < Number(argv[2])) {
+    let j = 0;
+    while (j < Number(argv[2])) {
+      x += 'X';
+      j++;
+    }
+    console.log(x);
+    x = '';
+    i++;
+  }
+}
