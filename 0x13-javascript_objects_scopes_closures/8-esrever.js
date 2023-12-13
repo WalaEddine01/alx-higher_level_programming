0b71187f7e78bd23daa11ccd9f088982711f1b63
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-13 23:36:34 +0100

    I made the changes on 2023-12-13 23:36:34

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
+  if (list.length === 0) { return; }
   let i; let n = list.length; let tmp;
   for (i = 0; i <= n / 2; i++) {
     tmp = list[n - 1];
--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const esrever = require('./8-esrever').esrever;
 
-console.log(esrever(["1", 2, { id: 3 }, 4, "5", 6]));
+console.log(esrever([]));
