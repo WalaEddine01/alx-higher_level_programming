c76209bed9bee4ab5f0753c4344b7d4a9bcdd4e7
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-13 23:43:26 +0100

    I made the changes on 2023-12-13 23:43:26

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const num = Number(process.argv[2]);
-if (!isNaN(num)) {
-  console.log(parseInt(num));
+const argv = process.argv;
+if (!isNaN(Number(argv[2]))) {
+  console.log(parseInt(Number(argv[2])));
 } else {
   console.log('Not a number');
 }
--- 0x12-javascript-warm_up/main.js
+++ 0x12-javascript-warm_up/main.js
@@ -1,27 +1,23 @@
 #!/usr/bin/node
-function Circle1(radius)
-{
-    return {
-        radius,
-        draw: function()
-        {
-            console.log('draw');
-        }
-    };
+function Circle1 (radius) {
+  return {
+    radius,
+    draw: function () {
+      console.log('draw');
+    }
+  };
 }
-const Circle =  Circle1(1);
+const Circle = Circle1(1);
 Circle.draw();
 console.log(Circle);
 console.log(Circle.radius);
 console.log('------------------');
 
-function Circle2(radius)
-{
-    this.radius = radius;
-    this.draw = function draw()
-    {
-        console.log('draw');
-    }
+function Circle2 (radius) {
+  this.radius = radius;
+  this.draw = function draw () {
+    console.log('draw');
+  };
 }
 const circle = new Circle2(1);
 circle.draw();
