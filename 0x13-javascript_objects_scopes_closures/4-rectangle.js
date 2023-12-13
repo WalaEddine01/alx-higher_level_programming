449348fa45975d681315f2761f6abd3f89994e8c
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-13 22:13:16 +0100

    I made the changes on 2023-12-13 22:13:16

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let i = 0; let j = 0;
+    let X = '';
+    for (i = 0; i < this.height; i++) {
+      X = '';
+      for (j = 0; j < this.width; j++) {
+        X += 'X';
+      }
+      console.log(X);
+    }
+  }
+  rotate () {
+    let a = this.height;
+    this.height = this.width;
+    this.width = a;
+  }
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/main.js
+++ 0x13-javascript_objects_scopes_closures/main.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
-const Rectangle = require('./3-rectangle');
+const Rectangle = require('./4-rectangle');
 
 const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
+console.log('Normal:');
+r1.print();
+
+console.log('Double:');
+r1.double();
+r1.print();
+
+console.log('Rotate:');
+r1.rotate();
 r1.print();
