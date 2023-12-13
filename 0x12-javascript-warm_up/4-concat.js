09bdecc683125399698343c83adc0dc2672b2d36
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-13 23:39:50 +0100

    I made the changes on 2023-12-13 23:39:50

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const argv = process.argv;
-console.log(argv[2], ' is ', argv[3]);
+console.log(argv[2], 'is', argv[3]);
--- 0x12-javascript-warm_up/main.js
+++ 0x12-javascript-warm_up/main.js
@@ -1,3 +1,29 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
-console.log(add(3, 5));
+function Circle1(radius)
+{
+    return {
+        radius,
+        draw: function()
+        {
+            console.log('draw');
+        }
+    };
+}
+const Circle =  Circle1(1);
+Circle.draw();
+console.log(Circle);
+console.log(Circle.radius);
+console.log('------------------');
+
+function Circle2(radius)
+{
+    this.radius = radius;
+    this.draw = function draw()
+    {
+        console.log('draw');
+    }
+}
+const circle = new Circle2(1);
+circle.draw();
+console.log(circle);
+console.log(circle.radius);
