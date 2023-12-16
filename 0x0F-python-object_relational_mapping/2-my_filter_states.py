f2615208f3e678173be9928fcdd56f4a8da85749
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-16 16:27:18 +0100

    I made the changes on 2023-12-16 16:27:18

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
+"""states models
 """
-this Module lists the states that matches with the user input
-"""
-
 if __name__ == "__main__":
     import MySQLdb
-    from sys import argv
+    import sys
 
-    user = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    state_name = argv[4]
-
-    conn = MySQLdb.connect(
-        host="localhost",
-        user=user,
-        passwd=password,
-        db=db_name,
-        port=3306
+    db_host = "localhost"
+    db_user = sys.argv[1]  # "your_username"
+    db_password = sys.argv[2]  # "your_password"
+    db_name = sys.argv[3]  # "your_database_name"
+    port = 3306
+    state_name = sys.argv[4]  # "your_database_name"
+    query = "SELECT * FROM states WHERE name LIKE BINARY\
+ '{}' ORDER BY id ASC".format(state_name)
+    db = MySQLdb.connect(
+        host=db_host, user=db_user, passwd=db_password, db=db_name, port=port
     )
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
-                '{}' ORDER BY states.id ASC".format(state_name))
+    cursor = db.cursor()
 
-    rows = cur.fetchall()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
     for row in rows:
         print(row)
-    cur.close()
-    conn.close()
+
+    cursor.close()
+    db.close()
