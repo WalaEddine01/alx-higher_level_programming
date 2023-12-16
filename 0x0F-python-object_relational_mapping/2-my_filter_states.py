77058d589abb68e7cbde47e95696550069713ab6
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-16 16:04:21 +0100

    I made the changes on 2023-12-16 16:04:21

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+this Module lists the states that matches with the user input
+"""
+
+if (__name__ == "__main__"):
+    import MySQLdb
+    from sys import argv
+
+    user = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        user=user,
+        passwd=password,
+        db=db_name,
+        port=3306
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name like\
+                '{}' ORDER BY id ASC".format(state_name))
+
+    for row in cur:
+        print(row)
+    cur.close()
+    conn.close()
