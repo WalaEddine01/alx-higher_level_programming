93119d0274473489389616e0f3847c409bfe1752
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-16 16:11:38 +0100

    I made the changes on 2023-12-16 16:11:38

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,25 +3,20 @@
 this Module lists the states that matches with the user input
 """
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     import MySQLdb
     from sys import argv
 
-    user = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    state_name = argv[4]
-
     conn = MySQLdb.connect(
         host="localhost",
-        user=user,
-        passwd=password,
-        db=db_name,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
         port=3306
     )
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name like BINARY\
-                '{}' ORDER BY states.id ASC".format(state_name))
+                '{}' ORDER BY states.id ASC".format(argv[4]))
 
     for row in cur:
         print(row)
