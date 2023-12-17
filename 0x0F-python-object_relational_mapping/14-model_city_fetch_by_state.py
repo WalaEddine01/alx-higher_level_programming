d82feae1a3d49cc0c3b11a3539130452346fda0e
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-17 21:21:46 +0100

    I made the changes on 2023-12-17 21:21:45

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,7 +6,8 @@ objects from the database hbtn_0e_14_usa
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
-from model_state import City, Base
+from model_city import City, Base
+from model_state import State
 
 if __name__ == "__main__":
     url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
@@ -14,6 +15,9 @@ if __name__ == "__main__":
     Base.metadata.create_all(bind=engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    result = session.query(City).all()
-    for r in result:
-        print(str(r.id) + ": " + r.name)
+    result = session.query(City, State).\
+        join(State, State.id == City.state_id).all()
+    if result:
+        for city, state in result:
+            print(f"{state.name}: ({city.id}) {city.name}")
+    session.close()
