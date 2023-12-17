6991f6988403be6f548f754320357b1a203fff89
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-17 17:28:58 +0100

    I made the changes on 2023-12-17 17:28:58

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -21,8 +21,8 @@ class State(Base):
                 unique=True, autoincrement=True)
     name = Column("name", String(128), nullable=False)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
     engine = create_engine(url=url, echo=True)
     Base.metadata.create_all(bind=engine)
