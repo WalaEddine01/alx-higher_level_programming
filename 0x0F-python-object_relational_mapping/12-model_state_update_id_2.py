e922e14b268db7c5110ca9a57a7d351c337ca6d3
WalaEddine01 <walaaeddine33@gmail.com>
2023-12-17 20:44:25 +0100

    I made the changes on 2023-12-17 20:44:25

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains a script that changes the name of a
+State object from the database hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import State, Base
+
+if __name__ == "__main__":
+    url = f"mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    engine = create_engine(url=url, echo=False)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id)\
+        .filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
+    session.close()
