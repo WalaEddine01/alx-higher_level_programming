00a07d3c1e2698508192f3b3e3b125823b1a61dc
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-06 13:06:32 +0100

    I made the changes on 2024-01-06 13:06:32

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept.
-curl -sI 0.0.0.0:5000/route_4 | grep Allow | cut -d ":" -f2 | sed "s. .."
+curl -sI "$1" | grep Allow | cut -d ":" -f2 | sed "s. .."
