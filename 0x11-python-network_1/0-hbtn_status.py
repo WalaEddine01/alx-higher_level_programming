04c5be2591fdd4a7c82552dfce4268614e085eb0
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-06 20:30:04 +0100

    I made the changes on 2024-01-06 20:30:04

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
+import urllib.request
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as resp:
+    response = resp.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
+    print(f"\t- utf8 content: {response.decode('utf-8')}")
