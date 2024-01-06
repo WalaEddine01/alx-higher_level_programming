951781840549bb89c17a097999863a7a54465334
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-06 14:30:04 +0100

    I made the changes on 2024-01-06 14:30:04

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#displays the body of the response with a header variable X-School-User-Id must be sent with the value 98.
-curl -sH "X-School-User-Id: 98" "$1"
+#displays the body of the response with a header variable
+curl -s -H "X-School-User-Id: 98" "$1"
