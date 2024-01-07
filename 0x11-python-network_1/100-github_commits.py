7b000de48cc7f52a1cd67e4a2f2e031bca1ef517
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-07 22:20:29 +0100

    I made the changes on 2024-01-07 22:20:29

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,15 +8,12 @@ from sys import argv
 
 if __name__ == "__main__":
     url = f'https://api.github.com/repos/{argv[1]}/{argv[2]}/commits'
+    req = requests.get(url)
+    commits = req.json()
     try:
-        req = requests.get(url)
-    except requests.exceptions.ConnectionError as e:
-        print(e)
-    try:
-        commits = req.json()
-    except ValueError:
-        print("Not a valid JSON")
-    for j in range(10):
-        print("{}: {}".format(
-            commits[j].get('sha'),
-            commits[j].get('commit').get('author').get('name')))
+        for j in range(10):
+            print("{}: {}".format(
+                commits[j].get('sha'),
+                commits[j].get('commit').get('author').get('name')))
+    except IndexError:
+        pass
