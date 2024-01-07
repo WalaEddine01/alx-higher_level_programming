614fb09513158462a687b82b85656ab53f697607
WalaEddine01 <walaaeddine33@gmail.com>
2024-01-07 22:22:35 +0100

    I made the changes on 2024-01-07 22:22:35

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    url = f'https://api.github.com/repos/{argv[1]}/{argv[2]}/commits'
+    url = f'https://api.github.com/repos/{argv[2]}/{argv[1]}/commits'
     req = requests.get(url)
     commits = req.json()
     try:
